ba9adeabbbb0dd17aa11dc9cfb14d6890313d014
Raúl Caro Pastorino <dev@fryntiz.es>
2021-06-21 00:12:58 +0200

    Added correct name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'es.desdechipiona.aviones',
-  appName: 'app-airflight-realtime-chipiona',
+  appName: 'Vuelos en Chipiona',
   webDir: 'dist',
   bundledWebRuntime: false
 };
